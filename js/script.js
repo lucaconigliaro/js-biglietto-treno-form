ba7e0d8044928b60807416a25d204f069a9c6f30
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-08 18:21:40 +0100

    feat: card problem

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 const formInput = document.getElementById("user-form");
 const kmInput = document.getElementById("km");
 const ageInput = document.getElementById("age");
+const usernameInput = document.getElementById("username")
 const pricePerKm = 0.21;
 const discount18 = 20;
 const discountOver65 = 40;
 // console.log(formInput, kmInput, ageInput)
 
-formInput.addEventListener("submit", function(event) {
+// Elementi della card
+const userElem = document.getElementById("username-card");
+const offerElem = document.getElementById("offer-card")
+const carriageElem = document.getElementById("carriage-card")
+const codeElem = document.getElementById("code-card")
+const ticketPriceElem = document.getElementById("ticket-price-card")
+const cardElem = document.getElementById("card-ticket")
+
+
+formInput.addEventListener("submit", function (event) {
     event.preventDefault()
     const userAge = parseInt(ageInput.value.trim());
     const userKm = parseInt(kmInput.value.trim());
@@ -26,12 +36,15 @@ formInput.addEventListener("submit", function(event) {
 function ticketPrice(age, km, costKm, discount18, discount65) {
 
     let ticketPrice = km * costKm;
-    if(age < 18) {
+    if (age < 18) {
         const discountCalc18 = ticketPrice / 100 * discount18;
         ticketPrice = discountCalc18;
     } else if (age > 65) {
         const discountCalc65 = ticketPrice / 100 * discount65;
         ticketPrice = discountCalc65;
     }
-   return ticketPrice.toFixed(2);
+    return ticketPrice.toFixed(2);
+    userElem.innerHTML = username;
+    cardElem.classList.remove("d-none")
+
 }
